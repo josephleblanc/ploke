37b1c05192d8a00ba5846dc5cde517126b35e5b4
Joseph LeBlanc <joseph.t.leblanc@protonmail.com>
2025-04-09 14:52:48 +0000

    new fixture with some of the old target files

--- tests/fixture_crates/fixture_nodes/src/lib.rs
+++ tests/fixture_crates/fixture_nodes/src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod const_static;
+mod enums;
+mod impls;
+mod imports;
+mod macros;
+mod structs;
+mod traits;
+mod type_alias;
+mod unions;
